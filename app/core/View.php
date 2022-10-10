912ad1d97f46ba18cf9ff4d9922c39e9ff45067a
Evgenij Frolov <evgenij.frolov@privatbank.ua>
2022-10-10 17:35:46 +0300

    add suffix to template file

--- app/core/View.php
+++ app/core/View.php
@@ -6,19 +6,19 @@ namespace core;
 
 class View
 {
-    const VIEW_DIR = 'app/source/views';
+    const VIEWS_DIR = 'app/source/views';
     protected $template = 'index_template';
 
     public function __construct(string $template = null)
     {
         if (!empty($template)) {
-            $this->template = $template;
+            $this->template = $template.'_template';
         }
     }
 
     public function render(string $page, array $options = [])
     {
         extract($options);
-        include_once self::VIEWS_DIR . '/templates' . $this->template . '.php';
+        include_once self::VIEWS_DIR . '/templates/' . $this->template . '.php';
     }
 }
