344929c4f8a7d02fe84b4c970c32a93d2b76d990
Evgenij Frolov <evgenij.frolov@privatbank.ua>
2022-10-11 23:42:24 +0300

    ArticleModel

---
+++ app/models/ArticleModel.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace models;
+
+
+class ArticleModel
+{
+    /**
+     * @var \mysqli
+     */
+    protected $db;
+
+    /**
+     * @var string table name
+     */
+    protected $table='articles';
+
+    /**
+     * ArticleModel constructor.
+     */
+    public function __construct()
+    {
+        $this->db = new \mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+        if ($this->db->connect_error!=0){
+            die($this->db->connect_error);// TODO добавить исключение
+        }
+    }
+
+    /**
+     * add new article in storage
+     * @param array $article associated array of article params
+     * @return bool|\mysqli_result
+     */
+    public function  add(array $article){
+        $sql = "INSERT INTO {$this->table}(title, text) VALUES ('{$article['title']}','{$article['text']}');";
+        return $this->db->query($sql);
+    }
+}
