0dc20dbbad62d9ecd540ff93f7985327ff793053
Evgenij Frolov <evgenij.frolov@privatbank.ua>
2022-10-12 16:44:42 +0300

    all articles show

--- app/source/views/pages/admin_index_page.php
+++ app/source/views/pages/admin_index_page.php
@@ -1,1 +1,23 @@
-<h2>All articles CRUD</h2>
+<h2>All articles</h2>
+<a href="<?= \core\Route::url('admin', 'create') ?>">create article</a>
+<table>
+    <thead>
+    <tr>
+        <th>#</th>
+        <th>Title</th>
+        <th>Action</th>
+    </tr>
+    </thead>
+    <tbody>
+        <?php if (!empty($articles)): ?>
+            <?php foreach ($articles as $article): ?>
+                <tr>
+                    <td><?= $article['id'] ?></td>
+                    <td><?= $article['title'] ?></td>
+                    <td><!--SDSD--></td>
+                </tr>
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </tbody>
+</table>
+
